30c8c7a6f172f5045863443cbd3bb07c335d72a9
sodaler <misha_lector87@mail.ru>
2022-07-17 21:25:09 +0400

    optimize done

--- laravel_vue/resources/js/components/IndexComponent.vue
+++ laravel_vue/resources/js/components/IndexComponent.vue
@@ -14,14 +14,7 @@
             <tbody>
             <!-- template - tag without built-in scripts (neutral)-->
             <template v-for="person in people">
-                <tr :class="isEdit(person.id) ? 'd-none' : ''">
-                    <th scope="row">{{ person.id }}</th>
-                    <td>{{ person.name }}</td>
-                    <td>{{ person.age }}</td>
-                    <td>{{ person.job }}</td>
-                    <td><a href="#" @click.prevent="changeEditPersonId(person.id, person.name, person.age, person.job)" class="btn btn-success">Edit</a></td>
-                    <td><a href="#" @click.prevent="deletePerson(person.id)" class="btn btn-danger">Delete</a></td>
-                </tr>
+                <ShowComponent :person="person" :ref="`show_${person.id}`"></ShowComponent>
                 <EditComponent :person="person" :ref="`edit_${person.id}`"></EditComponent>
             </template>
             </tbody>
@@ -31,6 +24,7 @@
 
 <script>
 import EditComponent from "./EditComponent";
+import ShowComponent from "./ShowComponent";
 export default {
     name: "IndexComponent",
 
@@ -49,7 +43,8 @@ export default {
     },
 
     components: {
-        EditComponent
+        EditComponent,
+        ShowComponent
     },
 
     methods: {
